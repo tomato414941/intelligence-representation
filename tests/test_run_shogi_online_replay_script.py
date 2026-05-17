9ba8d31d70e0383a7748b327a03a534cda9df741
tomato414941 <tomato414941@gmail.com>
2026-05-17 12:14:05 +0000

    tune: lower online replay gate workers

--- tests/test_run_shogi_online_replay_script.py
+++ tests/test_run_shogi_online_replay_script.py
@@ -49,7 +49,7 @@ class RunShogiOnlineReplayScriptTest(unittest.TestCase):
 
         config = run_replay.call_args.args[0]
         self.assertEqual(config.training_budget.sampled_examples_per_iteration, 32768)
-        self.assertEqual(config.generator_gate_worker_processes, 32)
+        self.assertEqual(config.generator_gate_worker_processes, 8)
         self.assertEqual(config.generation_worker_processes, 8)
 
     def test_passes_cli_arguments_to_online_replay_config_and_prints_result(self) -> None:
